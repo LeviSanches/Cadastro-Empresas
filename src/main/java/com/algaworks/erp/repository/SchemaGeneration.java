0d2ecc112b7d19db7d646a209b0f661ec3a52278
Levi Sanches <levi_sanches@outlook.com>
2023-07-15 12:08:15 -0300

    fix: javax para jakarta

--- src/main/java/com/algaworks/erp/repository/SchemaGeneration.java
+++ src/main/java/com/algaworks/erp/repository/SchemaGeneration.java
@@ -2,9 +2,9 @@ package com.algaworks.erp.repository;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 import com.algaworks.erp.model.Empresa;
 
